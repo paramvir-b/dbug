4c2265f96361d07d1e7f7fa8e791bc38ac39138f
paramvir-b <paramvir@rokoder.com>
2013-01-01 21:09:49 -0600

    Update sample

--- sample/Sample.c
+++ sample/Sample.c
@@ -2,11 +2,29 @@
 #include <stdlib.h>
 #include "../dbug/dbug.h"
 
+void testFunction_2(int arg1, int arg2, int arg3) {
+    DBUG_ENTER("TESTFUNCTION_2");
+
+    DBUG_PRINT("ARGS", ("arg1=%d arg2=%d arg3=%d", arg1, arg2, arg3));
+
+    DBUG_PRINT("TRACE", ("This is a trace message"));
+    DBUG_PRINT("DEBUG", ("This is a debug message"));
+    DBUG_PRINT("DEBUG1", ("This is a debug message"));
+    DBUG_PRINT("DEBUG2", ("This is a debug message"));
+    DBUG_PRINT("INFO", ("This is an info message"));
+    DBUG_PRINT("WARN", ("This is a warn message"));
+    DBUG_PRINT("ERROR", ("This is an error message"));
+    DBUG_PRINT("FATAL", ("This is a fatal message"));
+
+    DBUG_VOID_RETURN;
+}
+
 void testFunction(int arg1, int arg2) {
     DBUG_ENTER("TESTFUNCTION");
 
     DBUG_PRINT("ARGS", ("arg1=%d arg2=%d", arg1, arg2));
 
+    testFunction_2(7, 9, 100);
     DBUG_PRINT("TRACE", ("This is a trace message"));
     DBUG_PRINT("DEBUG", ("This is a debug message"));
     DBUG_PRINT("DEBUG1", ("This is a debug message"));
